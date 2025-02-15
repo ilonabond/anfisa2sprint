44650fbe20398930585ad40281d08a748c506834
Илона Бондар <ilona_bondars@mail.ru>
2025-02-15 11:52:03 +0300

    all

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -7,15 +7,27 @@ class Category(PublishedModel):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
     output_order = models.PositiveSmallIntegerField(default=100)
+   
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
 
 class Topping(PublishedModel):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
 
+    class Meta:
+        verbose_name = 'топпинги'
+        verbose_name_plural = 'Топпинги'
+
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, help_text='Уникальное название обёртки, не более 256 символов')
+
+    class Meta:
+        verbose_name = 'обёртки'
+        verbose_name_plural = 'Обёртки'
 
 
 class IceCream(PublishedModel):
@@ -36,3 +48,6 @@ class IceCream(PublishedModel):
     toppings = models.ManyToManyField(Topping)
     is_on_main = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name = 'мороженое'
+        verbose_name_plural = 'Мороженое'
